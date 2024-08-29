37239d4a51a8349226a215c51bf8fe1a72876df8
Brenda Chirata <--global>
2024-08-29 15:28:14 +0200

    first commit docker react webapp

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello there Brenda Chirata.you go girl
+        </p>
+        <a
+          className="App-link"
+          BYE there
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
